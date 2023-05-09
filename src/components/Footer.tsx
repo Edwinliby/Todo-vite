641ce0c62126bd2f81e17074f826cc355b81c76c
eKuttan <77681229+Edwinliby@users.noreply.github.com>
2023-05-10 03:37:31 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
 
 export default function Footer() {
   return (
-    <div className="absolute w-full text-center bottom-0 p-2">
+    <div className="absolute w-full flex justify-between bottom-0 p-2">
       Made with ❤️ by <a href="https://github.com/Edwinliby">eKuttan</a>
+<button
+    onClick={deleteAccount}
+    className='text-black font-semibold absolute bottom-3 right-3 flex gap-1 items-center w-full justify-end'>
+    <TbTrash size="25" className="text-red-600 hover:scale-105" /> Delete Account
+</button>
     </div>
   )
 }
