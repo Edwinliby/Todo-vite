eade0640de81b267a65860b8d9a82542e0afbe0b
eKuttan <77681229+Edwinliby@users.noreply.github.com>
2023-05-10 03:35:25 +0530

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -72,11 +72,7 @@ function Home() {
                 onComplete={toggleTaskCompletedById}
             />
 
-            <button
-                onClick={deleteAccount}
-                className='text-black font-semibold absolute bottom-3 right-3 flex gap-1 items-center w-full justify-end'>
-                <TbTrash size="25" className="text-red-600 hover:scale-105" /> Delete Account
-            </button>
+            
             <Footer/>
         </>
     )
